926a7131a6195f5c1dd081db279c8e12c9c455bf
logan <logan@logans-MacBook-Pro.local>
2020-09-03 13:31:10 -0500

    second try

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ $(function () {
         //add a new li using our textVal
         $(".shopping-list").append("<li> <span class='shopping-item'>" + textVal + "</span> <div class='shopping-item-controls'> <button class='shopping-item-toggle'> <span class='button-label'>check</span> </button> <button class='shopping-item-delete'> <span class='button-label'>delete</span> </button> </div> </li>");
 
+        // QUESTION HERE - HOW CAN I STRUCTURE THIS SO THAT THE EVENTS WILL FIRE ON ORIGINAL ITEMS & THE APPENDED ITEMS
+        // I WANT TO AVOID THE DUPLICATION I HAVE...
+
         //delete the div and using .on applies it to the newly appended items also
         $('.shopping-item-delete').on('click', function (event) {
             $(this).closest("li").remove();
@@ -20,7 +23,21 @@ $(function () {
         $('.shopping-item-toggle').on('click', function (event) {
             $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
         });
+
     });
+
+    //HERE YOU WILL SEE I DUPLICATED THE CODE FOR THE EVENTS - SO THAT IT CAN APPLY TO ORIGINAL LIST ITEMS
+    
+    //delete the div and using .on applies it to the newly appended items also
+    $('.shopping-item-delete').on('click', function (event) {
+        $(this).closest("li").remove();
+    });
+
+    //toggle class and using .on applies it to the newly appended items also
+    $('.shopping-item-toggle').on('click', function (event) {
+        $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
+    });
+
 
 });
 
